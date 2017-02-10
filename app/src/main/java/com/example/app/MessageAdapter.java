f167a8a971854c8f6c2ae571f21141f1fdc42a19
jcoro <jcoronite@hotmail.com>
2017-02-10 11:36:40 -0700

    Add main files

---
+++ app/src/main/java/com/example/app/MessageAdapter.java
@@ -0,0 +1,49 @@
+package com.example.app;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.app.FriendlyMessage;
+
+import java.util.List;
+
+public class MessageAdapter extends ArrayAdapter<FriendlyMessage> {
+    public MessageAdapter(Context context, int resource, List<FriendlyMessage> objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = ((Activity) getContext()).getLayoutInflater().inflate(com.example.app.udacity.friendlychat.R.layout.item_message, parent, false);
+        }
+
+        ImageView photoImageView = (ImageView) convertView.findViewById(com.example.app.udacity.friendlychat.R.id.photoImageView);
+        TextView messageTextView = (TextView) convertView.findViewById(com.example.app.udacity.friendlychat.R.id.messageTextView);
+        TextView authorTextView = (TextView) convertView.findViewById(com.example.app.udacity.friendlychat.R.id.nameTextView);
+
+        FriendlyMessage message = getItem(position);
+
+        boolean isPhoto = message.getPhotoUrl() != null;
+        if (isPhoto) {
+            messageTextView.setVisibility(View.GONE);
+            photoImageView.setVisibility(View.VISIBLE);
+            Glide.with(photoImageView.getContext())
+                    .load(message.getPhotoUrl())
+                    .into(photoImageView);
+        } else {
+            messageTextView.setVisibility(View.VISIBLE);
+            photoImageView.setVisibility(View.GONE);
+            messageTextView.setText(message.getText());
+        }
+        authorTextView.setText(message.getName());
+
+        return convertView;
+    }
+}
